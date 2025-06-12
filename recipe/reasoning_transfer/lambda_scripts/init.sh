e7a7e7b3660f8e683daba9cf8d0fa4f4450bc630
jackcai1206 <jackcai1206@gmail.com>
2025-06-12 16:26:57 +0000

    refactor: update training scripts for improved performance and configuration

---
+++ recipe/reasoning_transfer/lambda_scripts/init.sh
@@ -0,0 +1,29 @@
+source .venv/bin/activate
+python -m ensurepip
+# uv pip install torch torchvision torchaudio
+# uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128 --python-version 3.10 --only-binary=:all:
+
+uv pip install wheel
+uv pip install flash-attn==2.7.3 --no-build-isolation 
+# python -m pip install flash-attn --index-url https://pypi.jetson-ai-lab.dev/sbsa/cu128
+uv pip install flashinfer-python==0.2.2
+
+uv pip install liger-kernel
+
+uv pip install vllm==0.8.5
+# python -m pip install xgrammar vllm --index-url https://pypi.jetson-ai-lab.dev/sbsa/cu128/
+
+uv pip install -e .
+
+uv pip install jupyter ipykernel matplotlib
+
+uv pip install hf_transfer
+
+
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+# sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cudnn9-cuda-12
+uv pip install --no-build-isolation transformer_engine[pytorch]
+uv pip install megatron-core
--- recipe/reasoning_transfer/train_megatron.sh
+++ recipe/reasoning_transfer/train_megatron.sh
@@ -2,58 +2,53 @@ set -x
 
 # If you are using vllm<=0.6.3, you might need to set the following environment variable to avoid bugs:
 # export VLLM_ATTENTION_BACKEND=XFORMERS
-# export RAY_DEBUG_POST_MORTEM=1
+# export RAY_DEBUG_POST_MORTEM=1 
+export CUDA_DEVICE_MAX_CONNECTIONS=1 # For megatron communication/computation overlapping
 
-# model_name="Qwen/Qwen3-0.6B"
-model_name="meta-llama/Llama-3.2-3B-Instruct"
+model_name="Qwen/Qwen3-0.6B"
 experiment_name=$model_name
 
 train_files="['$HOME/Jack/data/verl/jackcai1206_sudoku_easy2hard/train.parquet']"
 val_files="['$HOME/Jack/data/verl/jackcai1206_sudoku_easy2hard/test.parquet', '$HOME/Jack/data/verl/HuggingFaceH4_aime_2024/train.parquet', '$HOME/Jack/data/verl/HuggingFaceH4_MATH-500/test.parquet', '$HOME/Jack/data/verl/MathArena_aime_2025/train.parquet']"
 
-tp_size=1
-sp_size=1
+tp_size=8
 
 export VERL_LOGGING_LEVEL=INFO
 export VLLM_LOGGING_LEVEL=INFO
 
-python3 -m verl.trainer.main_ppo \
+python3 -m verl.trainer.main_ppo --config-path=config \
+    --config-name='ppo_megatron_trainer.yaml'\
     algorithm.adv_estimator=grpo \
     data.train_files="$train_files" \
     data.val_files="$val_files" \
     data.train_batch_size=512 \
-    data.max_prompt_length=700 \
+    data.max_prompt_length=650 \
     data.max_response_length=4096 \
     data.filter_overlong_prompts=True \
     data.filter_overlong_prompts_workers=16 \
     data.truncation='error' \
     actor_rollout_ref.model.path="$model_name" \
-    actor_rollout_ref.model.use_fused_kernels=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
-    actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=True \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=16384 \
-    actor_rollout_ref.actor.ulysses_sequence_parallel_size=${sp_size} \
-    actor_rollout_ref.rollout.disable_log_stats=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=False \
+    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=1 \
+    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=${tp_size} \
+    actor_rollout_ref.actor.use_dynamic_bsz=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=${tp_size} \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.val_kwargs.top_p=0.95 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.7 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    actor_rollout_ref.ref.ulysses_sequence_parallel_size=${sp_size} \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=1 \
+    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=${tp_size} \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
